057f19e0fa15d4bc8d14db77257df2010256ab18
Pijld <Danielpijl2001@gmail.com>
2023-09-11 15:11:47 +0200

    add class

--- file1.py
+++ file1.py
@@ -1,2 +1,11 @@
-print("Hello world.")
-print("Hello world.")
+class group:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def sayhello(self):
+        print(f"hello, my name is {self.name} and i am {self.age} years old.")
+
+s1 = group("Sam", 20)
+s2 = group("Pam", 18)
+
+s1.sayhello()
