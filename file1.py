1103454fdc58a73aa154689a4142327818c5800c
Pijld <Danielpijl2001@gmail.com>
2023-10-20 22:37:46 +0200

    toevoegen van functies. versturen naar database.

--- file1.py
+++ file1.py
@@ -1,37 +1,81 @@
 import random
 import datetime
+import psycopg2
 
 current_time = datetime.datetime.now()
-print(current_time.strftime("%x " + "%H:" + "%M:"))
+real_time = current_time.strftime("%x " + "%H:" + "%M")
 
 bad_words = ("kut", "fuck", "shit", "hoer", "tering")
 
-max_character = 140
-min_character = 1
 
-stations = ["Almere", "Amersfoort", "Utrecht"]
-
-
-name = input("enter your name: ")
-if name == "":
-    print("anonymous \n")
-else:
-    print(name + "\n") #z
+def input_name():
+    name = input("enter your name: ")
+    if name == "":
+        name = "anonymous"
+    return name
 
 
-while True:
-    message = input("enter a message (max 140 characters): ")
-    if min_character <= len(message) <= max_character:
-        if any(word in message for word in bad_words):
-            print("no bad words please.")
+def input_message():
+    max_character = 140
+    min_character = 1
+    while True:
+        name = input_name()
+        message = input("enter a message (max 140 characters): ")
+        if min_character <= len(message) <= max_character:
+            with open("output.txt", "a") as f:
+                f.write(f"name: {str(name)} ; message: {str(message)}  datetime: {str(real_time)} \n")
+            return message
+        elif min_character > len(message):
+            print("please enter a message, containing at least 1 character.")
         else:
-            print(message + "\n")
-            with open("output.txt", "w") as outputfile:
-                outputfile.write(message + " \nname: " + str(name))
-            break
-    elif min_character > len(message):
-        print("please enter a message, containing at least 1 character.")
-    else:
-        print("please enter a maximum of 140 characters.")
+            print("please enter a maximum of 140 characters.")
 
-print(f"printed out on {random.choice(stations)} station")
+
+def read_message():
+    with (open("output.txt", "r") as f):
+        lines = f.readlines()
+        line = lines[-1] if lines else None
+        words = line.split()
+        return words
+
+
+def kies_station():
+    with open("stations.txt", "r") as f:
+        lines = f.readlines()
+        random_station = random.choice(lines)
+        return random_station
+
+
+def conn_database():
+    gekozen_station = kies_station()
+    lijn = read_message()
+
+    conn = psycopg2.connect(host="20.229.131.82", dbname="Data messages", user="postgres", password="postgres")
+    cur = conn.cursor()
+
+    create_table = """CREATE TABLE IF NOT EXISTS berichten(
+    id SERIAL PRIMARY KEY,
+    naam VARCHAR(255),
+    bericht VARCHAR(141), 
+    station VARCHAR(255),
+    datum VARCHAR(255))"""
+
+    insert_table = """INSERT INTO berichten(naam, bericht, station, datum) VALUES(%s, %s, %s, %s)"""
+    insert_value = (lijn[1], lijn[4], gekozen_station, lijn[6])
+
+    cur.execute(create_table)
+    cur.execute(insert_table, insert_value)
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+# def test():
+#     msg = read_message()
+#     word = msg[1]
+#     print(word)
+
+
+input_message()
+kies_station()
+conn_database()
