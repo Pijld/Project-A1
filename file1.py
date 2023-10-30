75e104dd0bb9d9d023ec1c45dda61d0b337a1ba0
Pijld <Danielpijl2001@gmail.com>
2023-10-30 17:10:39 +0100

    30-10-2023

--- file1.py
+++ file1.py
@@ -2,9 +2,6 @@ import random
 import datetime
 import psycopg2
 
-current_time = datetime.datetime.now()
-real_time = current_time.strftime("%x " + "%H:" + "%M")
-
 bad_words = ("kut", "fuck", "shit", "hoer", "tering")
 
 
@@ -16,6 +13,9 @@ def input_name():
 
 
 def input_message():
+    current_time = datetime.datetime.now()
+    real_date = current_time.date()
+    real_time = current_time.strftime("%H:" + "%M")
     max_character = 140
     min_character = 1
     while True:
@@ -23,7 +23,7 @@ def input_message():
         message = input("enter a message (max 140 characters): ")
         if min_character <= len(message) <= max_character:
             with open("output.txt", "a") as f:
-                f.write(f"name ; {str(name)} ; message ; {str(message)} ; datetime ; {str(real_time)} \n")
+                f.write(f"name ; {str(name)} ; message ; {str(message)} ; datetime ; {real_date} ; {real_time}\n")
             return message
         elif min_character > len(message):
             print("please enter a message, containing at least 1 character.")
@@ -46,15 +46,25 @@ def kies_station():
         return random_station
 
 
-def conn_database():
+def connect_database():
     goedgekeurd = True
+
     gekozen_station = kies_station()
     lijn = read_message()
     if any(word in lijn[3] for word in bad_words):
         goedgekeurd = False
 
-    conn = psycopg2.connect(host="20.229.131.82", dbname="Data messages", user="postgres", password="postgres")
+    gekozen_mod = random.randint(1, 4)
+
+    conn = psycopg2.connect(host="20.229.131.82", dbname="stationszuil", user="postgres", password="postgres")
     cur = conn.cursor()
+
+    cur.execute('DROP TABLE IF EXISTS moderators')
+
+    create_mod = """CREATE TABLE IF NOT EXISTS moderators(
+    id INT PRIMARY KEY, 
+    naam VARCHAR(255),
+    email VARCHAR(255)) """
 
     create_table = """CREATE TABLE IF NOT EXISTS berichten(
     id SERIAL PRIMARY KEY,
@@ -62,23 +72,36 @@ def conn_database():
     bericht VARCHAR(141), 
     station VARCHAR(255),
     datum VARCHAR(255),
-    status VARCHAR(255))"""
+    status VARCHAR(255),
+    tijd VARCHAR(255),
+    moderator INT) """
+
+    # fk_mod = """"ALTER TABLE berichten ADD CONSTRAINT fk_mod
+    # FOREIGN KEY REFERENCES(moderator) REFERENCES moderators.id"""
+
+    insert_mod = """INSERT INTO moderators(naam, email, id)
+    VALUES(%s, %s, %s)"""
+    mod_values = [("Jan Versteeg", "Janversteeg@moderator.com", 1), ("Henk de Jong", "HenkdeJong@moderator.com", 2),
+                  ("Pam de Haan", "PamdeHaan@moderator.com", 3), ("Youri Lang", "YouriLang@moderator.com", 4)]
 
     if goedgekeurd:
-        insert_table = """INSERT INTO berichten(naam, bericht, station, datum, status) VALUES(%s, %s, %s, %s, %s)"""
-        insert_value = (lijn[1], lijn[3], gekozen_station, lijn[5], "goedgekeurd")
+        insert_table = """INSERT INTO berichten(naam, bericht, station, datum, tijd, status, moderator) 
+        VALUES(%s, %s, %s, %s, %s, %s, %s)"""
+        insert_value = (lijn[1], lijn[3], gekozen_station, lijn[5], lijn[6], "goedgekeurd", gekozen_mod)
     else:
-        insert_table = """INSERT INTO berichten(naam, bericht, station, datum, status) VALUES(%s, %s, %s, %s, %s)"""
-        insert_value = (lijn[1], lijn[3], gekozen_station, lijn[5], "afgekeurd")
+        insert_table = """INSERT INTO berichten(naam, bericht, station, datum, tijd, status, moderator) 
+        VALUES(%s, %s, %s, %s, %s, %s, %s)"""
+        insert_value = (lijn[1], lijn[3], gekozen_station, lijn[5], lijn[6], "afgekeurd", gekozen_mod)
 
+    cur.execute(create_mod)
     cur.execute(create_table)
+    # cur.execute(fk_mod)
+    cur.executemany(insert_mod, mod_values)
     cur.execute(insert_table, insert_value)
     conn.commit()
-
     cur.close()
     conn.close()
 
 
 input_message()
-kies_station()
-conn_database()
+connect_database()
