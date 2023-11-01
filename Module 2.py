6b1e87f5c34f6bd2862f1821b07554748f87dcf5
Pijld <Danielpijl2001@gmail.com>
2023-11-01 18:51:28 +0100

    module 2. login succesful

--- Module 2.py
+++ Module 2.py
@@ -1,10 +1,12 @@
 from tkinter import *
+
 window = Tk()
 window.geometry("642x400")
 window.resizable(False, False)
 
 window.title("inlogscherm")
-# icon =
+icon = PhotoImage(file="nslogo.png")
+window.iconphoto(True, icon)
 
 canvas = Canvas(window, bg='#FFFFFF', width=642, height=400)
 canvas.pack()
@@ -26,9 +28,27 @@ email_input.place(x=210, y=200, height=25, width=410)
 wachtwoord_input = Entry(window, bg="#dedbde", font=("Arial", 12), show="*")
 wachtwoord_input.place(x=210, y=270, height=25, width=410)
 
+foutmelding_text = Label(window, text="", font=("Arial, 10"), bg="#FFC917", fg="red")
+foutmelding_text.place(x=210,y=300,height=20,width=400)
+
 def new_window():
+    email = email_input.get(1.0, "end-1c")
+    wachtwoord = wachtwoord_input.get()
+    with open("moderators.txt", "r") as f:
+        for line in f:
+            words = line.strip().split(";")  # remove newline and split into words
+            if words[1].strip() == email and words[2].strip() == wachtwoord:
+                open_window = Tk()
+                window.destroy()
+            # else:
+            #     if window.winfo_exists():
+            #         foutmelding_text.config(text="onjuist emailadres of wachtwoord.")
+
 
 login_button = Button(window,text="Log in", font=("Open Sans", 16, "bold"), bg="#0063D3", fg="white",
-                      command=new_window)
+                      activebackground="#0063D3", activeforeground="white",command=new_window)
+login_button.place(x=332,y=350, height=40,width=200)
 
 window.mainloop()
+
+#bron end-1c chat gpt. betekent, end -1 character, om nieuwe line weg te halen.
